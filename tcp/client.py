ccfbad5f534f4b0f2033abc779c293980d2fb748
cch01 <chanchiho01@gmail.com>
2020-11-27 23:33:27 +0800

    cleanup 2

--- tcp/client.py
+++ tcp/client.py
@@ -1,11 +1,12 @@
 import socket, struct, cv2, base64, numpy, datetime
 
 
-structFormat = "<L"
+STRUCT_FORMAT = "<L"
 PORT = 1234
 HOST = 'localhost' #change to server / 'localhost' if not using vm
+PAYLOAD_SIZE = struct.calcsize(STRUCT_FORMAT)
+
 data = b''
-PAYLOAD_SIZE = struct.calcsize(structFormat)
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((HOST, PORT))
 frameCounter = 1
@@ -16,7 +17,7 @@ while True:
         if not packet: break
         data += packet
     packedMsg = data[:PAYLOAD_SIZE]
-    msgSize = struct.unpack(structFormat, packedMsg)[0]
+    msgSize = struct.unpack(STRUCT_FORMAT, packedMsg)[0]
 
     data = data[PAYLOAD_SIZE:]
 
